81f154ba8b2a4ee9b8920706e4073a23d7dbfbbc
Marija Marinkov <Marija.Marinkov@eiskonzept.com>
2024-05-29 14:55:10 +0200

    v1.5

---
+++ theDecoratorPattern/src/CondimentDecorator.java
@@ -0,0 +1,10 @@
+public class CondimentDecorator extends Beverage {
+    public String getDescription() {
+        return null;
+    }
+
+    @Override
+    public double cost() {
+        return 0;
+    }
+}
